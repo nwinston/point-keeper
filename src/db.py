e7ba0803dec96f8728bf1954ece1c5b04c243ca4
nwinston <nwinston96@gmail.com>
2019-11-12 07:02:34 -0500

    cleanup

--- src/db.py
+++ src/db.py
@@ -1,11 +1,3 @@
-'''
-import os
-import pickle
-import shutil
-import tempfile
-from collections import Counter
-from threading import Lock
-'''
 from collections import Counter
 import redis
 
@@ -13,13 +5,10 @@ import redis
 class DB:
 	def __init__(self, db_url):
 		self.conn = redis.from_url(db_url, decode_responses=True)
-		self.conn.flushdb()
 
 	def add_point(self, user):
 		points = self.conn.get(user)
-		print(points)
 		points = points + 1 if points else 1
-		print('post points: {}'.format(points))
 		self.conn.set(user, points)
 
 	def remove_point(self, user):
@@ -37,59 +26,3 @@ class DB:
 
 		results = Counter({key: self.conn.get(key) for key in keys})
 		return results.most_common(n)
-
-
-
-
-'''
-class DB:
-	def __init__(self, db_file):
-		self.counter = Counter()
-		self.db_file = db_file
-		self.lock = Lock()
-
-		if os.path.exists(self.db_file):
-			self._load(self.db_file)
-
-	def lock_db(f):
-		def wrapper(self, *args, **kwargs):
-			rv = None
-			with self.lock:
-				rv = f(self, *args, **kwargs)
-			return rv
-		return wrapper
-
-	@lock_db
-	def _load(self, database):
-		with open(self.db_file, 'rb') as db:
-			self.counter = pickle.load(db)
-
-	@lock_db
-	def save(self):
-		if not os.path.exists(self.db_file):
-			with open(self.db_file, 'wb') as f:
-				pickle.dump(self.counter, f)
-			return
-
-		with tempfile.NamedTemporaryFile('wb') as temp:
-			shutil.copyfile(self.db_file, temp.name)
-
-			with open(self.db_file, 'wb') as f:
-				try:
-					pickle.dump(self.counter, f)
-				except:
-					shutil.copyfile(temp.name, self.db_file)
-
-	def add_point(self, user):
-		self.counter[user] += 1
-
-	def remove_point(self, user):
-		if self.counter[user] >= 1:
-			self.counter[user] -= 1
-
-	def get_points(self, user=None, n=None):
-		if user:
-			return self.counter[user]
-
-		return self.counter.most_common(n)
-'''
